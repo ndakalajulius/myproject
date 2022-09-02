0ef8b3c51a50227e7d6531e4e4125f5c283bc8a1
ndakalajulius <julius.ndakala@student.moringaschool>
2022-09-02 08:17:07 +0300

    added function to fetch weather

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let weather ={
-    "apiKey":"6a530a631c600d8bf9f22eca9678984a"  
+    "apiKey":"6a530a631c600d8bf9f22eca9678984a",
+    fetchWeather:function(){("https://api.openweathermap.org/data/2.5/weather?q=Nairobi,Kenya&units=metric&APPID=6a530a631c600d8bf9f22eca9678984a")
+    .then((response)=>response.json()).then
+}
 }
