4b1a36ca533fd4d95f9d3c565821f81704828bf7
ndakalajulius <julius.ndakala@student.moringaschool>
2022-09-02 09:00:10 +0300

    added fetch the 'url'

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let weather ={
     "apiKey":"6a530a631c600d8bf9f22eca9678984a",
-    fetchWeather:function(){("https://api.openweathermap.org/data/2.5/weather?q=Nairobi,Kenya&units=metric&APPID=6a530a631c600d8bf9f22eca9678984a")
-    .then((response)=>response.json()).then
-}
-}
+    fetchWeather:function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+    + city 
+    + "&units=metric&APPID="
+    +this.apiKey
+
+    )
+
+    .then((response)=>response.json())
+    .then((data)=> console.log(data))
+},
+
+};
