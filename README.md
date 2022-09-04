8428ee5be16c3603cd46257244e9ff58967cbe9c
ndakalajulius <julius.ndakala@student.moringaschool>
2022-09-04 13:35:59 +0300

    added readme.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Weather App
+Weather app is an app that swearches for weather from different cities in the world.
+
+## Learning Goals
+Building a web app that using HTML/CSS/JS front-end that accesses data from a public
+API. All interactions between the client and the API being handled
+asynchronously and use JSON as the communication format.
+## Introduction
+Hello user, let's build a simple app that enables us to know different  cities' weather.
+
+# Getting Started
+ Create a html page and name it index.html
+ In the page create a class card where the search and button are placed
+ create another class weather. Inside put city, temperature,humidity and windy.
+ Use css to modify the html page
+ In the javascript part, define weather using let
+ Take ip key from openweather .org website after signing in.
+ use the fetch function
+ add event listeners for button and search bar.
+
+# Weather App
+Weather app is an app that searches for weather from different cities in the world.
+
+## Learning Goals
+Building a web app that using HTML/CSS/JS front-end that accesses data from a public
+API. All interactions between the client and the API being handled
+asynchronously and use JSON as the communication format.
+## Introduction
+Hello user, let's build a simple app that enables us to know different  cities' weather.
+ 
+ ## API reference
+ API key I used : 6a530a631c600d8bf9f22eca9678984a
+ 
+
+## Authors
+julius ndakala
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let weather ={
-    "apiKey":"6a530a631c600d8bf9f22eca9678984a",
-    fetchWeather:function(city){
+
+let weather = {
+    apiKey: "6a530a631c600d8bf9f22eca9678984a",
+    fetchweather: function(city){
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
     + city 
     + "&units=metric&APPID="
@@ -9,7 +10,40 @@ let weather ={
     )
 
     .then((response)=>response.json())
-    .then((data)=> console.log(data))
+    .then((data)=> this.displayWeather(data))
 },
 
+
+displayWeather:function(data) {
+    const {name}=data;
+    const {icon,description}= data.weather[0];
+    const {temp,humidity}=data.main;
+    const {speed}=data.wind;
+    
+
+    document.querySelector(".city").innerText= "weather in" + name;
+    document.querySelector(".description").innerText= description;
+    document.querySelector(".temperature").innerText= temp+ " ºC ";
+    document.querySelector(".humidity").innerText= " + Humidity:" + humidity + " % ";
+    document.querySelector(".windy").innerText= "wind speed: " + speed + " km/h ";
+},
+
+search : function (){
+    this.fetchweather(
+        document.querySelector (".search-bar").value);
+    
+},
+     
 };
+document
+.querySelector(".search-button")
+.addEventListener("click",function(){
+   // console.log("test")
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+if (event.key == "Enter"){
+    weather.search();
+}
+})
+
